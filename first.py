94a9de1b6b6ec78910affd2845be361d47381391
M-Zaain-Ul-Abideen <mzainkhan40@gmail.com>
2024-11-07 10:29:01 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,44 @@
+# a = 1
+# a = 8
+
+# print(a + b)
+
+# First value => varaible 'a' 
+# Second value => varaible 'b'
+
+# Data Type:
+# integers => 1, 2, 3,...
+# string => 'abcdefg' or "abcdefg564651"
+# name: "Abdullah"
+
+
+firstName = "Abdullah"
+lastName = "Soomro"
+# THis variable use for full name of the user
+fullName = firstName + ' ' + lastName
+
+# Abdullah Soomro
+# Abdullah's
+# Abdullah, sjlfal
+# print(firstName)
+
+
+a = "Abdullah,"
+b = "Zain's"
+c = 'Hamza\'s'
+d = "Ali"
+
+# print(b)
+
+num1 = int(input())
+# # user input => string
+# num2 = int(input())
+
+# print(num1)
+
+# print(num1 + 2)
+
+
+# number 1: 12
+# number 2: 1
+# Total: 254
